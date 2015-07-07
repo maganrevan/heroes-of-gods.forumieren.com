5408304a0bc5a5fc32e14a198b9ddf781cfec940
maganrevan <magnus.kruschwitz@outlook.com>
2015-07-07 11:45:39 +0200

    add-ranges
    add-ranges

--- heroes-of-gods-globale-functions.js
+++ heroes-of-gods-globale-functions.js
@@ -1,6 +1,6 @@
-var oWarrior = {HP: 350, DEF: 15, MP: 20, ATK: 35, EXP: 0};
-var oArcher = {HP: 230, DEF: 10, MP: 45, ATK: 25, EXP: 0};
-var oMage = {HP: 175, DEF: 5, MP: 65, ATK: 10, EXP: 0};
+var oWarrior = {HP: 350, DEF: 15, MP: 20, ATK: 35, EXP: 0, HPR: 40, MPR: 20};
+var oArcher = {HP: 230, DEF: 10, MP: 45, ATK: 25, EXP: 0, HPR: 50, MPR: 30};
+var oMage = {HP: 175, DEF: 5, MP: 65, ATK: 10, EXP: 0, HPR: 30, MPR: 50};
 
 $(document).ready(function () {
 
@@ -64,30 +64,36 @@ $(document).ready(function () {
     };
 
 //leveldesign
-    var aWarriorLast = new Array(oWarrior.HP, oWarrior.MP, oWarrior.ATK, oWarrior.DEF, oWarrior.EXP);
-    var aArcherLast = new Array(oArcher.HP, oArcher.MP, oArcher.ATK, oArcher.DEF, oArcher.EXP);
-    var aMageLast = new Array(oMage.HP, oMage.MP, oMage.ATK, oMage.DEF, oMage.EXP);
+    var aWarriorLast = new Array(oWarrior.HP, oWarrior.MP, oWarrior.ATK, oWarrior.DEF, oWarrior.EXP, oWarrior.HPR, oWarrior.MPR);
+    var aArcherLast = new Array(oArcher.HP, oArcher.MP, oArcher.ATK, oArcher.DEF, oArcher.EXP, oArcher.HPR, oArcher.MPR);
+    var aMageLast = new Array(oMage.HP, oMage.MP, oMage.ATK, oMage.DEF, oMage.EXP, oMage.HPR, oMage.MPR);
     for (var i = 1; i < 101; i++) {
-        $('#warrior').children('tbody').append('<tr><td>' + i + '</td><td>' + aWarriorLast[0] + '</td><td>' + aWarriorLast[1] + '</td><td>' + aWarriorLast[2] + '</td><td>' + aWarriorLast[3] + '</td><td>' + aWarriorLast[4] + '</td></tr>');
+        $('#warrior').children('tbody').append('<tr><td>' + i + '</td><td>' + aWarriorLast[0] + '</td><td>' + aWarriorLast[1] + '</td><td>' + aWarriorLast[2] + '</td><td>' + aWarriorLast[3] + '</td><td>' + aWarriorLast[4] + '</td><td>'+aWarriorLast[5]+'</td><td>'+aWarriorLast[6]+'</td></tr>');
         aWarriorLast[0] += 57;
         aWarriorLast[1] += 5;
         aWarriorLast[2] += 25;
         aWarriorLast[3] += 29;
         aWarriorLast[4] += i + 150;
+        aWarriorLast[5] += i + 25;
+        aWarriorLast[6] += i + 10;
 
-        $('#archer').children('tbody').append('<tr><td>' + i + '</td><td>' + aArcherLast[0] + '</td><td>' + aArcherLast[1] + '</td><td>' + aArcherLast[2] + '</td><td>' + aArcherLast[3] + '</td><td>' + aArcherLast[4] + '</td></tr>');
+        $('#archer').children('tbody').append('<tr><td>' + i + '</td><td>' + aArcherLast[0] + '</td><td>' + aArcherLast[1] + '</td><td>' + aArcherLast[2] + '</td><td>' + aArcherLast[3] + '</td><td>' + aArcherLast[4] + '</td><td>'+ aArcherLast[5] +'</td><td>'+ aArcherLast[6] +'</td></tr>');
         aArcherLast[0] += 20;
         aArcherLast[1] += 13;
         aArcherLast[2] += 14;
         aArcherLast[3] += 11;
         aArcherLast[4] += i + 150;
+        aArcherLast[4] += i + 25;
+        aArcherLast[4] += i + 15;
 
-        $('#mage').children('tbody').append('<tr><td>' + i + '</td><td>' + aMageLast[0] + '</td><td>' + aMageLast[1] + '</td><td>' + aMageLast[2] + '</td><td>' + aMageLast[3] + '</td><td>' + aMageLast[4] + '</td></tr>');
+        $('#mage').children('tbody').append('<tr><td>' + i + '</td><td>' + aMageLast[0] + '</td><td>' + aMageLast[1] + '</td><td>' + aMageLast[2] + '</td><td>' + aMageLast[3] + '</td><td>' + aMageLast[4] + '</td><td>' + aMageLast[5] + '</td><td>' + aMageLast[6] + '</td></tr>');
         aMageLast[0] += 16;
         aMageLast[1] += 23;
         aMageLast[2] += 6;
         aMageLast[3] += 13;
         aMageLast[4] += i + 150;
+        aMageLast[4] += i + 10;
+        aMageLast[4] += i + 25;
     }
 
 });
